74af2f8897c41b87d14f7727a0a987adb2dc4d1a
Elias Soares <elias_soares95@hotmail.com>
2017-07-23 18:28:02 +0000

    Manipulação de erros gulp

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -8,5 +8,10 @@ cssImport = require('postcss-import');
 gulp.task('styles', function(){
     return gulp.src('./app/assets/styles/styles.css')
     .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    // Manipulação de erros
+    .on('error', function(errorInfo){
+        console.log(errorInfo.toString());
+        this.emit('end');
+    })
     .pipe(gulp.dest('./app/temp/styles'));
 });
