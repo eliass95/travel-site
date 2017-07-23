d4b83a8ce7374d065dec7e28fe53ee07af58d7df
Elias Soares <elias_soares95@hotmail.com>
2017-07-23 03:52:38 +0000

    Configuração import dos estilos

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,10 @@ watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested');
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
 
+/*
 gulp.task('default', function(){
     console.log("Hooray - you created a Gulp Task.");
 });
@@ -12,17 +14,20 @@ gulp.task('default', function(){
 gulp.task('html', function(){
     console.log("Imagine something useful being done to your html here.");
 })
+*/
 
 gulp.task('styles', function(){
     return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'));
 })
 
 gulp.task('watch', function(){
+    /*
     watch('./app/index.html', function(){
         gulp.start('html');
     });
+    */
 
     watch('./app/assets/styles/**/*.css', function(){
         gulp.start('styles');
