86cd14dee5f82e89ea75d924fa1c8548f17ea2f2
Elias Soares <elias_soares95@hotmail.com>
2017-08-20 01:54:32 +0000

    criação tasks gulp beginClean e endClean.

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
 svgSprite = require('gulp-svg-sprite'),
-rename = require('gulp-rename');
+rename = require('gulp-rename'),
+del = require('del');
 
 var config = {
     mode: {
@@ -15,21 +16,39 @@ var config = {
     }
 }
 
-gulp.task('createSprite', function(){
+// Deleta arquivos antigos antes de gerar novamente
+gulp.task('beginClean', function(){
+	return del(['./app/temp/sprite', './app/assets/images/sprites']);
+});
+
+// Depende de beginClean
+// Une vários arquivos svg em um só
+// Salva svg e css na pasta temp/sprite
+gulp.task('createSprite', ['beginClean'], function(){
     return gulp.src('./app/assets/images/icons/**/*.svg')
         .pipe(svgSprite(config))
         .pipe(gulp.dest('./app/temp/sprite/'));
 });
 
+// Depende de createSprite
+// Copia o svg de temp para assets
 gulp.task('copySpriteGraphic', ['createSprite'], function(){
 	return gulp.src('./app/temp/sprite/css/**/*.svg')
 		.pipe(gulp.dest('./app/assets/images/sprites'));
 });
 
+// Depende de createSprite
+// Copia css, renomeia e salva na pasta assets
 gulp.task('copySpriteCSS', ['createSprite'], function(){
     return gulp.src('./app/temp/sprite/css/*.css')
         .pipe(rename('_sprite.css'))
         .pipe(gulp.dest('./app/assets/styles/modules'));
+});
+
+// Deleta a pasta temp/sprite após copiar svg e css
+gulp.task('endClean', ['copySpriteGraphic','copySpriteCSS'], function(){
+	return del('./app/temp/sprite');
 })
 
-gulp.task('icons', ['createSprite', 'copySpriteGraphic','copySpriteCSS']);
+// Executa todas as tasks
+gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic','copySpriteCSS', 'endClean']);
