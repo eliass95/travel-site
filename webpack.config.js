87645580c0ffe42642ea8660267c41b6b7605504
Elias Soares <elias_soares95@hotmail.com>
2017-09-02 23:40:28 -0300

    Configuração babel.

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,16 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "./app/temp/scripts"),
         filename: "App.js"
+    },
+    // configuração babel
+    module: {
+        loaders: [{
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015']
+            },
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
     }
 }
